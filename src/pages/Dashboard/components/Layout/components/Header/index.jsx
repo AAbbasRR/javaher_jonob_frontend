d70465eec5305d54243b9da5a70e56214a691b17
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2024-05-26 01:31:41 +0330

    working on header

--- src/pages/Dashboard/components/Layout/components/Header/index.jsx
+++ src/pages/Dashboard/components/Layout/components/Header/index.jsx
@@ -1,71 +1,72 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import IconStaffManagement from "src/assets/icons/icon-admin-management.svg";
-import IconCustomerManagement from "src/assets/icons/icon-customer-management.svg";
-import IconHome from "src/assets/icons/icon-home.svg";
-import IconProductManagement from "src/assets/icons/icon-product-management.svg";
-import IconStoreManagement from "src/assets/icons/icon-store.svg";
+import IconExit from "src/assets/icons/icon-exit.svg";
+import IconLock from "src/assets/icons/icon-lock.svg";
+import { Button } from "src/components/Button";
+import { Modal } from "src/components/Modal";
 import useAuthStore from "src/store";
 import style from "./style.module.scss";
 
 const menu = [
 	{
-		title: "داشبورد",
-		icon: IconHome,
-		path: "dashboard",
-		permissions: ["staff", "superuser"],
-	},
-	{
-		title: "انبار",
-		icon: IconStoreManagement,
-		path: "store-management",
-		permissions: ["staff", "superuser"],
-	},
-	{
-		title: "محصولات",
-		icon: IconProductManagement,
-		path: "product-management",
-		permissions: ["staff", "superuser"],
-	},
-	{
-		title: "فاکتور",
-		icon: IconProductManagement,
-		path: "factor-management",
+		title: "تغییر رمزعبور",
+		icon: IconLock,
+		path: "change-password",
 		permissions: ["superuser", "staff", "secretary", "worker"],
-	},
-	{
-		title: "کارمندان",
-		icon: IconStaffManagement,
-		path: "staff-management",
-		permissions: ["superuser"],
-	},
-	{
-		title: "مشتریان",
-		icon: IconCustomerManagement,
-		path: "customer-management",
-		permissions: ["superuser", "staff", "secretary"],
 	},
 ];
 
 export const Header = () => {
 	const claassName = ({ isActive }) => (isActive ? style.nav__link + " active" : style.nav__link);
-	const { userInfo } = useAuthStore();
+	const { logout, userInfo } = useAuthStore();
+
+	const [open, setOpen] = useState(false);
+
+	const handleLogout = () => {
+		setOpen(false);
+		logout();
+	};
 
 	return (
-		<div className={style.wrapper}>
-			<div className={style.main}>
-				<nav className={style.nav}>
-					{menu.map((link, i) => {
-						if (link?.permissions.includes(userInfo.type)) {
-							return (
-								<NavLink key={i} to={link.path} end className={claassName}>
-									<img className={style.nav__linkIcon} src={link.icon} alt="icon" />
-									<span className={style.nav__linkTitle}>{link.title}</span>
-								</NavLink>
-							);
-						}
-					})}
-				</nav>
+		<>
+			<div className={style.wrapper}>
+				<div className={style.main}>
+					<nav className={style.nav}>
+						{menu.map((link, i) => {
+							if (link?.permissions.includes(userInfo.type)) {
+								return (
+									<NavLink key={i} to={link.path} end className={claassName}>
+										<img className={style.nav__linkIcon} src={link.icon} alt="icon" />
+										<span className={style.nav__linkTitle}>{link.title}</span>
+									</NavLink>
+								);
+							}
+						})}
+						<button className={style.nav__link} onClick={() => setOpen(true)}>
+							<img className={style.nav__linkIcon} alt="logout-icon" src={IconExit} />
+							<span className={style.nav__linkTitle}>خروج</span>
+						</button>
+					</nav>
+				</div>
 			</div>
-		</div>
+			<Modal
+				fullWidth
+				state={open}
+				setState={setOpen}
+				title="خروج"
+				footerEnd={
+					<div className={style.buttons}>
+						<Button size="xlarge" variant="ghost" onClick={() => setOpen(false)}>
+							انصراف
+						</Button>
+						<Button size="xlarge" onClick={handleLogout}>
+							تایید
+						</Button>
+					</div>
+				}
+			>
+				<div className={style.exit}>آیا مطمئن هستید که میخواهید خارج شوید؟</div>
+			</Modal>
+		</>
 	);
 };
--- src/pages/Dashboard/components/Layout/index.jsx
+++ src/pages/Dashboard/components/Layout/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
 import styles from "./style.module.scss";
 
 const Layout = () => {
@@ -9,6 +10,7 @@ const Layout = () => {
 
 	return (
 		<>
+			<Header setSidebar={setSidebar} />
 			<div className={styles.wrapper}>
 				<Sidebar sidebar={sidebar} setSidebar={setSidebar} />
 				<div className={styles.content}>
